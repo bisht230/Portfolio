f3f0c3f78ca9ab10dbd41df24baffa162eae6831
bisht230 <bishtdeepanshu007@gmail.com>
2023-01-06 20:05:56 +0530

    Changes in profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,5 @@
 import { Box, Flex, Heading, Text, useMediaQuery } from "@chakra-ui/react";
 import React from "react";
-
 const Profile = () => {
   const { isNotSmallerScreen } = useMediaQuery("(min-width:600px)");
   return (
