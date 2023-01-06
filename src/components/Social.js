d36202c4fc3c9fcdeb1d7791623bb8877ebe9f05
bisht230 <bishtdeepanshu007@gmail.com>
2023-01-06 16:21:18 +0530

    Changes in social.js

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,15 +1,23 @@
-import { HStack, Icon, Link } from '@chakra-ui/react'
-import React from 'react'
-import {FaGithub , FaInstagram , FaLinkedin } from 'react-icons/fa';
+import { HStack, Icon, Link } from "@chakra-ui/react";
+import React from "react";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const Social = () => {
   return (
     <HStack spacing="25">
-        <Link href="https://github.com/bisht230"><Icon as={FaGithub} boxSize="45"> </Icon></Link>
-        <Link href="https://www.linkedin.com/in/deepanshu-bisht-661785210/"><Icon as={FaLinkedin} boxSize="45"></Icon></Link>
-        <Link href=""><Icon as={FaInstagram} boxSize="45"></Icon></Link>
+      <Link href="https://github.com/bisht230">
+        <Icon as={FaGithub} boxSize="45">
+          {" "}
+        </Icon>
+      </Link>
+      <Link href="https://www.linkedin.com/in/deepanshu-bisht-661785210/">
+        <Icon as={FaLinkedin} boxSize="45"></Icon>
+      </Link>
+      <Link href="">
+        <Icon as={FaInstagram} boxSize="45"></Icon>
+      </Link>
     </HStack>
-  )
-}
+  );
+};
 
-export default Social
+export default Social;
