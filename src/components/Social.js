04930cc871b43e753caf7e1c539da6511b613a5b
bisht230 <bishtdeepanshu007@gmail.com>
2023-01-08 16:13:53 +0530

    Refactoring up the code

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,14 +1,18 @@
 import { HStack, Icon, Link } from "@chakra-ui/react";
 import React from "react";
-import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
-import{SiLeetcode , SiCodechef , SiCodeforces , SiGeeksforgeeks} from "react-icons/si";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import {
+  SiLeetcode,
+  SiCodechef,
+  SiCodeforces,
+  SiGeeksforgeeks,
+} from "react-icons/si";
 
 const Social = () => {
   return (
     <HStack spacing="25">
       <Link href="https://github.com/bisht230">
-        <Icon as={FaGithub} boxSize="35">
-        </Icon>
+        <Icon as={FaGithub} boxSize="35"></Icon>
       </Link>
       <Link href="https://www.linkedin.com/in/deepanshu-bisht-661785210/">
         <Icon as={FaLinkedin} boxSize="35"></Icon>
