3e631b34e9c7c5ee45e84ee4256eb72a263d0595
bisht230 <bishtdeepanshu007@gmail.com>
2023-01-19 21:45:08 +0530

    Changing the content

--- src/components/Header.js
+++ src/components/Header.js
@@ -55,18 +55,15 @@ const Header = () => {
               Deepanshu Bisht
             </Text>
             <Text color={isDark ? "gray.200" : "gray.500"}>
-             As an Information Technology engineering student, 
+            I am an Information Technology engineering student seeking opportunities for collaboration on web development projects.
               <Text color={isDark ? "gray.200" : "gray.500"}>
-              I am actively seeking opportunities for collaboration on web development projects.
+              I am a dedicated and enthusiastic learner, eager to work across technologies and domains.
               </Text>
               <Text color={isDark ? "gray.200" : "gray.500"}>
-              I am a dedicated learner who is eager to work across technologies and domains.
+              I have a keen interest in exploring new technologies and utilizing them to solve real-world problems.
               </Text>
               <Text color={isDark ? "gray.200" : "gray.500"}>
-              I have a passion for exploring new technologies and using them to solve real-world problems.
-              </Text>
-              <Text color={isDark ? "gray.200" : "gray.500"}>
-              Additionally, I enjoy mentoring and guiding others in their learning journey.
+              In addition, I am passionate about mentoring and guiding others in their learning journey. lems.
               </Text>
               Currently, I am focused on Full Stack Web Development and honing my skills in Data Structures and Algorithms.
             </Text>
